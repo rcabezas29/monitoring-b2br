c5df49bc926b388acd5e670793960843240704b3
Raúl Cabezas González <58788516+rcabezas29@users.noreply.github.com>
2021-05-28 16:23:21 +0200

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -3,11 +3,11 @@
 LVM=$(lvdisplay | awk 'NR == 8 {print $3}')
 
 echo "#Architechure: $(uname -a)"
-echo -n "#CPU pysical: "
+echo -n "#CPU physical: "
 lscpu | grep "CPU(s):" | awk 'NR == 1 {print $2}'
 echo -n "#vCPU: "
 lscpu | grep "socket" | awk '{print $4}'
-free --mega | awk 'NR == 2 {printf("#Memory usage: %i/%iMB (%.2f%%)\n"), $3, $2, $3/$2*100}'
+free --mega | awk 'NR == 2 {printf("#Memory usage: %i/%iMB (%.2f%%)\n"), $3, $2, $3/$2*$
 df -h | awk '$NF=="/"{printf "#Disk Usage: %i/%iGB (%s)\n", $3, $2, $5}'
 top -bn1 | grep load | awk '{printf "#CPU Load: %.2f\n", $(NF-2)}'
 who -b | awk '{printf "#Last boot: %s %s\n", $3, $4}'
@@ -23,4 +23,4 @@ w | wc -l | awk 'NR == 1 {printf "#User log: %i\n", $1 - 2}'
 echo -n "#NETWORK: IP "
 echo -n "$(hostname -I)"
 echo "($(cat /sys/class/net/enp0s3/address))"
-echo "#Sudo: $(cat /var/log/sudo.log | wc -l) cmd"
+echo "#Sudo: $(cat /var/log/sudo/sudo.log | grep "COMMAND" | wc -l) cmd"
