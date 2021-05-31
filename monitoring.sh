1c670fd3f648d6828dc9a8ae08f66c7a3aa42535
Raúl Cabezas González <58788516+rcabezas29@users.noreply.github.com>
2021-05-31 11:04:13 +0200

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-LVM=$(lvdisplay | awk 'NR == 8 {print $3}')
-
 echo "#Architechure: $(uname -a)"
 echo -n "#CPU physical: "
 lscpu | grep "CPU(s):" | awk 'NR == 1 {print $2}'
 echo -n "#vCPU: "
 lscpu | grep "socket" | awk '{print $4}'
-free --mega | awk 'NR == 2 {printf("#Memory usage: %i/%iMB (%.2f%%)\n"), $3, $2, $3/$2*$
+free --mega | awk 'NR == 2 {printf("#Memory usage: %i/%iMB (%.2f%%)\n"), $3, $2, $3/$2*100}'
 df -h | awk '$NF=="/"{printf "#Disk Usage: %i/%iGB (%s)\n", $3, $2, $5}'
-top -bn1 | grep load | awk '{printf "#CPU Load: %.2f\n", $(NF-2)}'
+top -bn 1 | awk 'NR == 3 {printf "#CPU Load: %.2f%%\n", $2+$4}'
 who -b | awk '{printf "#Last boot: %s %s\n", $3, $4}'
-echo -n "#LVM use: "
-if [ $LVM == "available" ]
-then
-        echo yes
-else
-        echo  no
-fi
+lvm pvdisplay | awk 'NR == 5 {printf "#LVM use: %s\n", $2}'
 ss -s | awk 'NR == 7 {printf "#Connections TCP: %i ESTABLISHED\n", $2}'
 w | wc -l | awk 'NR == 1 {printf "#User log: %i\n", $1 - 2}'
 echo -n "#NETWORK: IP "
